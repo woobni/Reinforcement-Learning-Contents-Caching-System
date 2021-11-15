2c32ab20d674adee23cc6a3fea4ac59a0afea3f1
woobni <woobin0357@gmail.com>
2021-11-15 21:47:01 +0900

    cache=10, contents=300, time_step=100 , user=2, fix a critic preference, first critic=0.5, optimal graph.

--- cachingSystem_env/gym_cachingSystem_env/envs/continuous_cache.py
+++ cachingSystem_env/gym_cachingSystem_env/envs/continuous_cache.py
@@ -22,7 +22,7 @@ class ContinuousCache(gym.Env):
         self.critics_num = 5
         self.critics = self.generate_critics()
         self.contents = self.generate_contents()
-        self.users_num = 4
+        self.users_num = 2  # 4
         self.users, self.users_preference = self.update_users()
         self.recommendation_list_size = 5  # number of contents recommended to users within cache.
         self.recommendation_list = self.update_recommendation_list()
@@ -97,15 +97,15 @@ class ContinuousCache(gym.Env):
     def update_users(self):
         # feature of users with expected ratings.
         users = []
-        preferences = []
-        # preferences = [1, 2]
+        # preferences = []
+        preferences = [1, 2]
         for i in range(self.users_num):
             # Suppose the users continues to change. (Random critic preference)
-            preference = np.random.randint(1, self.critics_num)
-            preferences.append(preference)
+            # preference = np.random.randint(1, self.critics_num)
+            # preferences.append(preference)
 
             # Suppose the users continues to remain unchanged. (Fix critic preference)
-            # preference = preferences[i]
+            preference = preferences[i]
 
             # first critic has universal content preference. so, fix to
             expected_ratings = [0.5*x + 0.5*y for x, y in zip(self.critics[0], self.critics[preference])]
--- caching_continuousActionSpace/main.py
+++ caching_continuousActionSpace/main.py
@@ -78,13 +78,7 @@ def model_train():
             epi_proposed_caching_score += reward
             epi_random_caching_score += env.random_caching()
             epi_popularity_caching_score += env.popularity_caching()
-            # epi_optimal_caching_score += env.optimal_caching()
-
-            # moving average score
-            # epi_proposed_caching_score = 0.9 * epi_proposed_caching_score + 0.1 * env.proposed_caching_score
-            # epi_random_caching_score = 0.9 * epi_random_caching_score + 0.1 * env.random_caching_score
-            # epi_popularity_caching_score = 0.9 * epi_popularity_caching_score + 0.1 * env.popularity_caching_score
-            # epi_optimal_caching_score = 0.9 * epi_optimal_caching_score + 0.1 * env.optimal_caching()
+            epi_optimal_caching_score += env.optimal_caching()
 
             # train models through memory replay
             agent.replay(1)
@@ -126,9 +120,9 @@ def model_train():
                 with random_caching_summary_writer.as_default():
                     tf.summary.scalar('epi_score', epi_random_caching_score, step=epi)
 
-                # with optimal_caching_summary_writer.as_default():
-                #     # tf.summary.scalar('epi_score', optimal_caching_score_mean, step=epi)
-                #     tf.summary.scalar('epi_score', epi_optimal_caching_score, step=epi)
+                with optimal_caching_summary_writer.as_default():
+                    # tf.summary.scalar('epi_score', optimal_caching_score_mean, step=epi)
+                    tf.summary.scalar('epi_score', epi_optimal_caching_score, step=epi)
 
                 break
 
