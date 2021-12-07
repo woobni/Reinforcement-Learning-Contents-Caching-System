5489f733d2602a7b0d9eb3c44865ea74b46877a5
woobni <woobin0357@gmail.com>
2021-12-08 03:59:26 +0900

    Add logger

--- cachingSystem_env/gym_cachingSystem_env/envs/continuous_cache.py
+++ cachingSystem_env/gym_cachingSystem_env/envs/continuous_cache.py
@@ -14,7 +14,7 @@ from gym import spaces
 class ContinuousCache(gym.Env):
     def __init__(self):
         self.step_size = 100
-        self.contents_total_num = 300
+        self.contents_total_num = 500
         self.cache_size = 10
         self.cache = self.cache_init_random()
         self.max_rating = 5  # max ratings that critics give to contents.
