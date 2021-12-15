b8d2513c827bb72f61222ba3065e6afb13638f83
woobni <woobin0357@gmail.com>
2021-12-15 16:43:33 +0900

    cache=20, contents=5000, popularity preference=0.5, user=4, recommend=10

--- cachingSystem_env/gym_cachingSystem_env/envs/continuous_cache.py
+++ cachingSystem_env/gym_cachingSystem_env/envs/continuous_cache.py
@@ -14,7 +14,7 @@ from gym import spaces
 class ContinuousCache(gym.Env):
     def __init__(self):
         self.step_size = 100
-        self.contents_total_num = 500
+        self.contents_total_num = 5000
         self.cache_size = 20
         self.cache = self.cache_init_random()
         self.max_rating = 5  # max ratings that critics give to contents.
