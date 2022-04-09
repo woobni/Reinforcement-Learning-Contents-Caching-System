2f026ff9a0770a28f8d1ccbd6ce137301917429a
woobni <woobin0357@gmail.com>
2022-04-09 21:27:22 +0900

    cache=10, contents=10000, popularity_preference=0.5, user=4, recommend=5, reward=proposed score

--- cachingSystem_env/gym_cachingSystem_env/__init__.py
+++ cachingSystem_env/gym_cachingSystem_env/__init__.py
@@ -6,16 +6,6 @@ register(
 )
 
 register(
-    id='foo-SelectSame-v0',
-    entry_point='gym_cachingSystem_env.envs:SelectSameEnv',
-)
-
-register(
-    id='foo-Action1-v0',
-    entry_point='gym_cachingSystem_env.envs:Action1Env',
-)
-
-register(
     id='foo-Test-v0',
     entry_point='gym_cachingSystem_env.envs:TestEnv',
 )
